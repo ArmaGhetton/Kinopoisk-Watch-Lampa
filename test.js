b212363afad57dc052c05d6df4ad0ed77d926710
ArmaGhetton <samotxave@gmail.com>
2026-07-06 03:22:28 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,26 @@
-(function () {
+function startPlugin() {
+    console.log('TEST PLUGIN STARTED');
 
-    Lampa.Noty.show('TEST PLUGIN LOADED');
-
-    console.log('TEST PLUGIN LOADED');
-
-    Lampa.Component.add('test_plugin', {
-        render: function () {
-            this.activity.loader(false);
-            this.activity.empty('Плагин работает!');
-        }
-    });
+    Lampa.Noty.show('TEST PLUGIN STARTED');
 
     var button = $(
         '<li class="menu__item selector">' +
-            '<div class="menu__ico">🧪</div>' +
-            '<div class="menu__text">Тест плагина</div>' +
+        '<div class="menu__ico">🧪</div>' +
+        '<div class="menu__text">TEST BUTTON</div>' +
         '</li>'
     );
 
     button.on('hover:enter', function () {
-        Lampa.Activity.push({
-            url: '',
-            title: 'Тест',
-            component: 'test_plugin'
-        });
+        Lampa.Noty.show('BUTTON WORKS');
     });
 
     $('.menu .menu__list').eq(0).append(button);
+}
 
-})();
+if (window.appready) {
+    startPlugin();
+} else {
+    Lampa.Listener.follow('app', function (e) {
+        if (e.type == 'ready') startPlugin();
+    });
+}
