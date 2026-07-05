39dec5290713ad60a3c1770ebc84a4b4c9aa9f99
ArmaGhetton <samotxave@gmail.com>
2026-07-06 00:28:37 +0700

    Add files via upload

---
+++ kinopoisk-watch.js
@@ -0,0 +1,115 @@
+/* global Lampa, $, window */
+
+(function () {
+    
+    console.log('Kinopoisk', 'PLUGIN LOADED');
+    
+    'use strict';
+
+    const WORKER_URL = 'https://kinopoisk-watch-api.samotxave.workers.dev';
+    const DEVICE_ID = '01b66544-35c7-4515-ac06-0d1b90574608';
+
+    function load(oncomplete) {
+
+   console.log('Kinopoisk', 'LOAD STARTED');
+		
+        fetch(
+            `${WORKER_URL}/watchlist?device_id=${DEVICE_ID}`
+        )
+        .then(r => r.json())
+        .then(data => {
+
+            const results = data.items
+                .filter(item => item.tmdb_full)
+                .map(item => item.tmdb_full);
+            
+            oncomplete({
+                page: 1,
+                results: results
+            });
+        })
+        .catch(() => {
+            oncomplete({
+                page: 1,
+                results: []
+            });
+        });
+
+			function clear() {}
+
+var Api = {
+    full: function(params, oncomplete, onerror) {
+        load(oncomplete);
+    },
+    clear: clear
+};
+
+    }
+
+    function component(object) {
+
+	console.log('Kinopoisk', 'COMPONENT CREATED');
+
+        const comp = new Lampa.InteractionCategory(object);
+
+comp.create = function() {
+    Api.full(
+        object,
+        this.build.bind(this),
+        this.empty.bind(this)
+    );
+};
+
+comp.nextPageRequest = function(
+    object,
+    resolve,
+    reject
+) {
+    Api.full(
+        object,
+        resolve.bind(comp),
+        reject.bind(comp)
+    );
+};
+
+        return comp;
+    }
+
+    function start() {
+
+        Lampa.Component.add(
+            'kinopoisk_watch',
+            component
+        );
+
+        const button = $(`
+            <li class="menu__item selector">
+                <div class="menu__text">
+                    Кинопоиск
+                </div>
+            </li>
+        `);
+
+        button.on('hover:enter', function () {
+            Lampa.Activity.push({
+                url: '',
+                title: 'Кинопоиск',
+                component: 'kinopoisk_watch',
+                page: 1
+            });
+        });
+
+        $('.menu .menu__list')
+            .eq(0)
+            .append(button);
+    }
+
+    if (window.appready) start();
+    else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') start();
+        });
+    }
+
+})();
+
