7a2d60f4186b38cd356e011880777f3c1ba9484e
ArmaGhetton <samotxave@gmail.com>
2026-07-06 04:01:31 +0700

    Update test.js

--- test.js
+++ test.js
@@ -2,27 +2,34 @@ function startPlugin() {
     console.log('TEST PLUGIN STARTED');
     Lampa.Noty.show('TEST PLUGIN STARTED');
 
-    Lampa.Listener.follow('full', function(e) {
+    Lampa.Listener.follow('full', function (e) {
         if (e.type === 'complite') {
-    
-    var button = $(
-        '<li class="menu__item selector">' +
-        '<div class="menu__ico">🧪</div>' +
-        '<div class="menu__text">АРМАГЕДДОН 999</div>' +
-        '</li>'
-    );
 
-    button.on('hover:enter', function () {
-        Lampa.Noty.show('BUTTON WORKS');
+            var button = $(
+                '<li class="menu__item selector">' +
+                    '<div class="menu__ico">🧪</div>' +
+                    '<div class="menu__text">АРМАГЕДДОН 999</div>' +
+                '</li>'
+            );
+
+            button.on('hover:enter', function () {
+                Lampa.Noty.show('BUTTON WORKS');
+            });
+
+            $('.menu .menu__list').eq(0).append(button);
+
+            console.log('BUTTON ADDED');
+            Lampa.Noty.show('BUTTON ADDED');
+        }
     });
-
-    $('.menu .menu__list').eq(0).append(button);
 }
 
 if (window.appready) {
     startPlugin();
 } else {
     Lampa.Listener.follow('app', function (e) {
-        if (e.type == 'ready') startPlugin();
+        if (e.type === 'ready') {
+            startPlugin();
+        }
     });
 }
